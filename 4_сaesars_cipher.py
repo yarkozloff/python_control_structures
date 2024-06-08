36f5052080a01fd4ee8ce8232c0fc61fbac3df39
kozlov.y@digdes.com <kozlov.y@digdes.com>
2024-06-08 18:58:58 +0300

    add 4 task

---
+++ 4_сaesars_cipher.py
@@ -0,0 +1,26 @@
+phrase = 'Caesar is the ruler of the Roman Empire'
+cipher = 29
+result = ""
+char_db_lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+char_db_upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+if cipher > len(char_db_lower):
+    cipher = cipher %26
+for char in phrase:
+    if char in char_db_lower:
+        if cipher + char_db_lower.index(char) >= 26:
+            lastidxchar = char_db_lower.index(char) - len(char_db_lower) + cipher
+            result += char_db_lower[lastidxchar]
+        else:
+            lastidxchar = char_db_lower.index(char) + cipher
+            result += char_db_lower[lastidxchar]
+    if char in char_db_upper:
+        if cipher + char_db_upper.index(char) >= 26:
+            lastidxchar = char_db_upper.index(char) - len(char_db_upper) + cipher
+            result += char_db_upper[lastidxchar]
+        else:
+            lastidxchar = char_db_upper.index(char) + cipher
+            result += char_db_upper[lastidxchar]
+    if char.isspace():
+        result += ' '
+print(result);
